c879dcfd80ff9e735774f2186f4d409cf65cb2f8
bluecoder@gmx.de <bluecoder@gmx.de@8f46629d-ec43-0410-9b68-4d101553c41d>
2008-05-19 18:21:03 +0000

    Added an ISO image.

--- images/images.sh
+++ images/images.sh
@@ -18,8 +18,8 @@ mkdosfs images/floppy_fat.img
 sudo mount -oloop images/floppy_ext2.img fd
 mkdir fd/boot
 mkdir fd/boot/grub
-cp compilers/dir/lib/grub/i386-pc/stage1 fd/boot/grub
-cp compilers/dir/lib/grub/i386-pc/stage2 fd/boot/grub
+cp compilers/dir/lib/grub/x86_64-unknown/stage1 fd/boot/grub
+cp compilers/dir/lib/grub/x86_64-unknown/stage2 fd/boot/grub
 cp images/menu.lst fd/boot/grub
 sudo umount fd
 
@@ -27,8 +27,8 @@ sudo umount fd
 sudo mount -oloop images/floppy_fat.img fd
 sudo mkdir fd/boot
 sudo mkdir fd/boot/grub
-sudo cp compilers/dir/lib/grub/i386-pc/stage1 fd/boot/grub
-sudo cp compilers/dir/lib/grub/i386-pc/stage2 fd/boot/grub
+sudo cp compilers/dir/lib/grub/x86_64-unknown/stage1 fd/boot/grub
+sudo cp compilers/dir/lib/grub/x86_64-unknown/stage2 fd/boot/grub
 sudo cp images/menu.lst fd/boot/grub
 sudo umount fd
 
@@ -38,3 +38,12 @@ sh images/grub.sh images/floppy_fat.img
 
 # Delete directory fd
 rmdir fd
+
+# Create an ISO image
+mkdir iso
+mkdir iso/boot
+mkdir iso/boot/grub
+cp compilers/dir/lib/grub/x86_64-unknown/stage2_eltorito iso/boot/grub
+cp images/menu.lst iso/boot/grub/menu.lst
+mkisofs -R -b boot/grub/stage2_eltorito -no-emul-boot -boot-load-size 4 -boot-info-table -o images/pedigree.iso iso
+rm -rf iso
