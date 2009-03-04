307eb248c8ab87da04254690767fd8a01c1a997b
mankeyrabbit <mankeyrabbit@8f46629d-ec43-0410-9b68-4d101553c41d>
2009-03-04 11:49:14 +0000

    Set debugger build to use -O2 - much faster.
    Fixed issue with missing ruby in r594, a lot of the applications and libraries needed were being cmake'd - these
    have now been moved into their own directory tree which is copied to the disk in create_images.sh.
    
    I may have borked the bashrc, it doesn't look as nice as before and has no $PATH. This will be changed back in the next revision.

--- scripts/create_images.sh
+++ scripts/create_images.sh
@@ -89,8 +89,10 @@ if which losetup >/dev/null 2>&1; then
   sudo cp $SRCDIR/libc.so $MOUNTPT/libraries
   sudo cp $SRCDIR/libm.so $MOUNTPT/libraries
 
+  sudo cp -r $SRCDIR/../images/i686-elf/* $MOUNTPT/
+
   sudo mkdir -p $MOUNTPT/etc/terminfo/v
-  sudo cp $SRCDIR/../scripts/termcap $MOUNTPT/etc
+#  sudo cp $SRCDIR/../scripts/termcap $MOUNTPT/etc
   sudo cp $SRCDIR/../scripts/vt100 $MOUNTPT/etc/terminfo/v/
 
   fini;
