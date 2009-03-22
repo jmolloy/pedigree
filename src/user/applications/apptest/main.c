da71ff678a33c0ce30434b1dac3953eebb218aa4
mankeyrabbit <mankeyrabbit@8f46629d-ec43-0410-9b68-4d101553c41d>
2009-03-22 10:47:05 +0000

    Changing apptest to be a socket app.

--- src/user/applications/apptest/main.c
+++ src/user/applications/apptest/main.c
@@ -1,61 +1,91 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
 #include <unistd.h>
-#include <sys/termios.h>
-#include <sys/stat.h>
+#include <stdio.h>
 
-int open(const char*,int,int);
+#define SOCKET_ERROR        -1
+#define BUFFER_SIZE         255
+#define HOST_NAME_SIZE      255
 
-int main(int argc, char **argv)
+#define htons(x) ( ((x&0xFF)<<8) | ((x&0xFF00)>>8) )
+
+int  main(int argc, char* argv[])
 {
-//  struct termios t;
-//  tcgetattr(0, &t);
-//  t.c_lflag &= ~ECHONL;
-//  tcsetattr(0, 0, &t);
+    int hSocket;                 /* handle to socket */
+    struct hostent *pHostInfo;   /* holds info about a machine */
+    struct sockaddr_in Address;  /* Internet socket address stuct */
+    long nHostAddress;
+    char pBuffer[BUFFER_SIZE];
+    unsigned nReadAmount;
+    char strHostName[HOST_NAME_SIZE];
+    int nHostPort;
 
-//  fprintf(stderr, "argc: %d, env: %s\n", argc, getenv("TERM"));
+    if(argc < 2)
+    {
+      printf("\nUsage: apptest host-name\n");
+      return 0;
+    }
+    else
+    {
+      strcpy(strHostName,argv[1]);
+      nHostPort=80;
+    }
 
-//  struct stat buf;
-//  int a = fstat(0,&buf);
+    printf("\nMaking a socket\n");
 
-//  fprintf(stderr, "fstat returned %d, mode: %x\n",a, buf.st_mode);
+    /* make a socket */
+    hSocket=socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
 
-//  int fd = open("./shell", 0, 0);
-//  fprintf(stderr, "stdin: %d, shell: %d\n", isatty(0), isatty(fd));
+    if(hSocket == SOCKET_ERROR)
+    {
+        printf("\nCould not make a socket\n");
+        return 0;
+    }
 
-//   int fd = open("/etc/passwd", 0, 0);
-//   unsigned int seed = 0x5a5a5a5a;
-//   unsigned char *buf = (unsigned char*)malloc(1026);
-//   buf[0] = 0xEE;
-//   buf[1025] = 0xEE;
-//   while (1)
-//   {
-//     seed ^= 0x462925a8;
-//     unsigned int start = seed&0xFF;
+    /* get IP address from name */
+    //pHostInfo=gethostbyname(strHostName);
+    /* copy address into long */
+    //memcpy(&nHostAddress,pHostInfo->h_addr_list[0],pHostInfo->h_length);
+    unsigned char ip[] = {172, 30, 1, 198};
+    memcpy(&nHostAddress,ip,4);
 
-//     lseek(fd, 0, 0);
-//     read(fd, &buf[1], 0x18);
+    /* fill address struct */
+    Address.sin_addr.s_addr=nHostAddress;
+    Address.sin_port=htons(nHostPort);
+    Address.sin_family=AF_INET;
 
-//     if (buf[0] != 0xEE)
-//     {
-//       fprintf(stderr, "Front corruption!: %x\n", buf[0]);
-//       exit(1);
-//     }
-//     if (buf[1025] != 0xEE)
-//     {
-//       fprintf(stderr, "Rear corruption!\n");
-//       exit(1);
-//     }
-// //    fprintf(stderr, "OK\n");
-//   }
+    printf("\nConnecting to %s on port %d\n",strHostName,nHostPort);
 
-  chdir("applications");
-  char b[512];
-  char *a = getcwd(b,512);
-  fprintf(stderr,"cwd: %s\n", b);
+    /* connect to host */
+    if(connect(hSocket,(struct sockaddr*)&Address,sizeof(Address))
+       == SOCKET_ERROR)
+    {
+        printf("\nCould not connect to host\n");
+        return 0;
+    }
 
-  fprintf(stderr,"attempting to page fault!\n");
-  char *x = (char*)0;
-  x[1] = 'p';
-  return 0xdeadbaba;
+    printf("Sending HTTP request...\n");
+
+    char *buffer = "GET / HTTP/1.0\r\n\r\n";
+
+    send(hSocket,buffer,strlen(buffer), 0);
+
+
+    /* read from socket into buffer
+    ** number returned by read() and write() is the number of bytes
+    ** read or written, with -1 being that an error occured */
+
+    nReadAmount=recv(hSocket,pBuffer,BUFFER_SIZE,0);
+    printf("\nReceived \"%s\" from server: %d\n",pBuffer,nReadAmount);
+
+    printf("\nClosing socket\n");
+    /* close socket */
+    if(close(hSocket) == SOCKET_ERROR)
+    {
+        printf("\nCould not close socket\n");
+        return 0;
+    }
 }
