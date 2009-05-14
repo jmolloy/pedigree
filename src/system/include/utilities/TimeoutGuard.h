0b2dcbc120b197d956d48a4ea62841ece3b8b1a9
James Molloy <james@jamesmolloy.co.uk>
2009-05-14 12:23:55 +0100

    Sorry, forgot to add two files

---
+++ src/system/include/utilities/TimeoutGuard.h
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) 2008 James Molloy, Jörg Pfähler, Matthew Iselin
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+#ifndef TIMEOUT_GUARD_H
+#define TIMEOUT_GUARD_H
+
+/**\file  TimeoutGuard.h
+ *\author James Molloy <jamesm@osdev.org>
+ *\date   Mon May 11 11:27:37 2009
+ *\brief  Implements the class TimeoutGuard, which cancels any operation after
+          a given amount of time. */
+
+#include <processor/types.h>
+#include <process/Event.h>
+#include <process/eventNumbers.h>
+#include <process/PerProcessorScheduler.h>
+#include <Spinlock.h>
+
+/** This class waits (in the background) for a given amount of time to elapse,
+    then cancels whatever operation is currently taking place.
+
+    The class functions in a similar way to setjmp/longjmp, except using
+    saveState/editState. The timeout is cancelled when the class goes out of
+    scope.
+
+    \note Because it uses 'longjmp', this class can cause memory leaks and 
+          skip destructors for stack-local objects. Use it wisely. */
+class TimeoutGuard
+{
+public:
+    /** Creates a new TimeoutGuard, with the given timeout, in seconds. */
+    TimeoutGuard(size_t timeoutSecs);
+
+    /** Destroys the TimeoutGuard, cancelling the timeout. */
+    ~TimeoutGuard();
+
+    /** Returns true if the guard just timed out, false if not. This should be
+        called just after the constructor. */
+    bool timedOut()
+    {return m_bTimedOut;}
+
+    /** Cancels the current operation.
+        \note This is intended only to be called from a TimeoutGuardEvent. */
+    void cancel();
+
+    /** Internal event class. */
+    class TimeoutGuardEvent : public Event
+    {
+    public:
+        TimeoutGuardEvent() :
+            Event(0, false), m_pTarget(0)
+        {}
+        TimeoutGuardEvent(TimeoutGuard *pTarget, size_t specificNestingLevel);
+        virtual ~TimeoutGuardEvent();
+
+        virtual size_t serialize(uint8_t *pBuffer);
+        static bool unserialize(uint8_t *pBuffer, TimeoutGuardEvent &event);
+
+        virtual size_t getNumber()
+        {return EventNumbers::TimeoutGuard;}
+
+        /** The TimeoutGuard to cancel. */
+        TimeoutGuard *m_pTarget;
+
+    private:
+        TimeoutGuardEvent(const TimeoutGuardEvent &);
+        TimeoutGuardEvent &operator = (const TimeoutGuardEvent &);
+    };
+
+private:
+    TimeoutGuard(const TimeoutGuard &);
+    TimeoutGuard &operator = (const TimeoutGuard &);
+
+    /** The event, which will be automatically freed when it fires. */
+    TimeoutGuardEvent *m_pEvent;
+
+    /** Did the operation time out? */
+    bool m_bTimedOut;
+
+    /** Saved state. */
+    SchedulerState m_State;
+
+    /** Saved nesting level. */
+    size_t m_nLevel;
+
+    /** Our own personal lock. */
+    Spinlock m_Lock;
+};
+
+#endif
---
+++ src/system/kernel/utilities/TimeoutGuard.cc
@@ -0,0 +1,108 @@
+/*
+ * Copyright (c) 2008 James Molloy, Jörg Pfähler, Matthew Iselin
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include <utilities/TimeoutGuard.h>
+#include <process/Thread.h>
+#include <process/PerProcessorScheduler.h>
+#include <processor/Processor.h>
+#include <machine/Timer.h>
+#include <machine/Machine.h>
+#include <LockGuard.h>
+#include <Log.h>
+
+TimeoutGuard::TimeoutGuard(size_t timeoutSecs) :
+    m_pEvent(0), m_bTimedOut(false), m_State(), m_nLevel(0), m_Lock()
+{
+    if (timeoutSecs)
+    {
+        Thread *pThread = Processor::information().getCurrentThread();
+        
+        m_nLevel = pThread->getStateLevel();
+        m_pEvent = new TimeoutGuardEvent(this, m_nLevel);
+        
+        Machine::instance().getTimer()->addAlarm(m_pEvent, timeoutSecs);
+        
+        // Generate the SchedulerState to restore to.
+        Processor::saveState(m_State);
+    }
+}
+
+TimeoutGuard::~TimeoutGuard()
+{
+    // Stop any interrupts - now we know that we can't be preempted by
+    // our own event handler.
+    LockGuard<Spinlock> guard(m_Lock);
+
+    // If the event hasn't fired yet, remove and delete it.
+    if (m_pEvent)
+    {
+        Machine::instance().getTimer()->removeAlarm(m_pEvent);
+        // Ensure that the event isn't queued.
+        Processor::information().getCurrentThread()->cullEvent(m_pEvent);
+        delete m_pEvent;
+        m_pEvent = 0;
+    }
+}
+
+void TimeoutGuard::cancel()
+{
+    // Called by TimeoutGuardEvent.
+    m_bTimedOut = true;
+
+    m_pEvent = 0;
+
+    Processor::restoreState(m_State);
+}
+
+static void guardEventFired(uint8_t *pBuffer)
+{
+    NOTICE("GuardEventFired");
+    TimeoutGuard::TimeoutGuardEvent e;
+    if (!TimeoutGuard::TimeoutGuardEvent::unserialize(pBuffer, e))
+    {
+        FATAL("guardEventFired: Event is not a TimeoutGuardEvent!");
+        return;
+    }
+    e.m_pTarget->cancel();
+    NOTICE("Cancel finished");
+}
+
+TimeoutGuard::TimeoutGuardEvent::TimeoutGuardEvent(TimeoutGuard *pTarget, size_t specificNestingLevel) :
+    Event(reinterpret_cast<uintptr_t>(&guardEventFired), true /* Deletable */, specificNestingLevel),
+    m_pTarget(pTarget)
+{
+}
+
+TimeoutGuard::TimeoutGuardEvent::~TimeoutGuardEvent()
+{
+}
+
+size_t TimeoutGuard::TimeoutGuardEvent::serialize(uint8_t *pBuffer)
+{
+    size_t *pBufferSize_t = reinterpret_cast<size_t*> (pBuffer);
+    pBufferSize_t[0] = EventNumbers::TimeoutGuard;
+    pBufferSize_t[1] = reinterpret_cast<size_t> (m_pTarget);
+    return 2*sizeof(size_t);
+}
+
+bool TimeoutGuard::TimeoutGuardEvent::unserialize(uint8_t *pBuffer, TimeoutGuardEvent &event)
+{
+    size_t *pBufferSize_t = reinterpret_cast<size_t*> (pBuffer);
+    if (pBufferSize_t[0] != EventNumbers::TimeoutGuard)
+        return false;
+    event.m_pTarget = reinterpret_cast<TimeoutGuard*> (pBufferSize_t[1]);
+    return true;
+}
