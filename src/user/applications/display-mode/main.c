8e7712d6caee330dd8931137f972336b83386f72
eddyb <eddy@eddy-pc.WORKGROUP>
2009-09-17 16:00:47 +0300

    We now have a working display mode changer app :D.

--- src/user/applications/display-mode/main.c
+++ src/user/applications/display-mode/main.c
@@ -19,54 +19,83 @@ int main(int argc, char **argv)
     }
 
     char buf[256];
-    int result, i;
+    int result;
+    unsigned int width, height, depth;
 
-    if(argc >= 2)
+    if(argc == 2 && !strcmp(argv[1], "list"))
     {
-        if(!strcmp(argv[1], "list"))
+        result = pedigree_config_query("select * from 'display-modes/0'");
+        if (result == -1)
+            fprintf(stderr, "Unable to get the modes.\n");
+
+        if (pedigree_config_was_successful(result) != 0)
         {
-            result = pedigree_config_query("select * from 'display-modes/0'");
+            pedigree_config_get_error_message(result, buf, 256);
+            fprintf(stderr, "Error: %s\n", buf);
+            return 1;
+        }
+
+        if (pedigree_config_numrows(result) == 0)
+            printf("No modes\n");
+        else
+            while (pedigree_config_nextrow(result) == 0)
+                printf("Mode %ux%ux%u\n", pedigree_config_getnum_s(result, "width"), pedigree_config_getnum_s(result, "height"), pedigree_config_getnum_s(result, "depth"));
+
+        pedigree_config_freeresult(result);
+
+    }
+    else if(argc == 3 && !strcmp(argv[1], "select"))
+    {
+        if(sscanf(argv[2], "%ux%ux%u", &width, &height, &depth) == 3)
+        {
+            sprintf(buf, "select * from 'display-modes/0' where width = %u and height = %u and depth = %u", width, height, depth);
+            result = pedigree_config_query(buf);
+            memset(buf, 0, 256);
             if (result == -1)
-            {
-                fprintf(stderr, "Unable to query.\n");
-            }
+                fprintf(stderr, "Unable to get the mode.\n");
 
             if (pedigree_config_was_successful(result) != 0)
             {
                 pedigree_config_get_error_message(result, buf, 256);
-                fprintf(stderr, "error: %s\n", buf);
+                fprintf(stderr, "Error: %s\n", buf);
                 return 1;
             }
 
-            if (pedigree_config_numrows(result) == 0)
+            if (pedigree_config_numrows(result) == 0 || pedigree_config_nextrow(result) != 0)
             {
-                // Empty set.
-                printf("No modes\n");
-                return 0;
+                pedigree_config_freeresult(result);
+                printf("No such mode\n");
             }
-
-            while (pedigree_config_nextrow(result) == 0)
+            else
             {
-                printf("Mode ");
                 memset(buf, 0, 256);
-                pedigree_config_getstr_s(result, "width", buf, 256);
-                printf("%sx", buf);
+                sprintf(buf, "update displays set mode_id = %u where id = 1", pedigree_config_getnum_s(result, "id"));
+                pedigree_config_freeresult(result);
+                printf("Setting mode %ux%ux%u...\n", width, height, depth);
+                result = pedigree_config_query(buf);
                 memset(buf, 0, 256);
-                pedigree_config_getstr_s(result, "height", buf, 256);
-                printf("%sx", buf);
-                memset(buf, 0, 256);
-                pedigree_config_getstr_s(result, "depth", buf, 256);
-                printf("%s", buf);
-                printf("\n");
-            }
-            pedigree_config_freeresult(result);
+                if (result == -1)
+                    fprintf(stderr, "Unable to set the mode.\n");
 
+                if (pedigree_config_was_successful(result) != 0)
+                {
+                    pedigree_config_get_error_message(result, buf, 256);
+                    fprintf(stderr, "Error: %s\n", buf);
+                    return 1;
+                }
+                pedigree_config_freeresult(result);
+            }
         }
         else
         {
             usage();
             exit(1);
         }
+    }
+    else
+    {
+        usage();
+        exit(1);
     }
 
     exit(0);
