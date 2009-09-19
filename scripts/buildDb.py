02cccc441ffa8720c65570e4124b9b78a2074e60
James Molloy <james@jamesmolloy.co.uk>
2009-09-19 13:50:18 +0100

    Added python build DB script, which is cleverer and puts tables first.

---
+++ scripts/buildDb.py
@@ -0,0 +1,47 @@
+#! /usr/bin/env python
+# coding=utf8
+
+#
+# Copyright (c) 2008 James Molloy, Jörg Pfähler, Matthew Iselin
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+
+import os
+import re
+import tempfile
+
+print "Building configuration database..."
+
+startdir = './src/'
+sql = ''
+tables = ''
+
+def walk(dummy, dirr, files):
+    for child in files:
+        if child == 'schema':
+            global sql
+            sql += open(dirr + '/' + child).read()
+
+os.path.walk(startdir, walk, 'null')
+
+m = re.findall('^create table .*?;$', sql, re.M|re.S|re.I)
+for match in m:
+    tables += match + '\n'
+
+# Gay -- can't use flags here so can't use case-insensitive or
+# dot-matches-all search.
+sql = re.sub('create table (.|\n)*?;', '', sql)
+sql = re.sub('CREATE TABLE (.|\n)*?;', '', sql)
+
+os.popen('sqlite3 ./build/config.db', 'w').write(tables+sql)
