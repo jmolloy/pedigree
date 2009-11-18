6e1c2cd6d3bfc70f04cac12ef671233d8acae255
Matthew Iselin <matthew@theiselins.net>
2009-11-18 17:21:54 +1000

    Added a generic PCI interface that each architecture will provide its own implementation for. This interface allows only access to the configuration space (to keep things simple). I may move it out of a driver and into the kernel proper in order to keep inter-module dependencies to a minimum.

---
+++ src/modules/drivers/common/pci-common/pci.h
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2008 James Molloy, Jörg Pfähler, Matthew Iselin
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#ifndef PCI_COMMON_H
+#define PCI_COMMON_H
+
+#include <Log.h>
+#include <Module.h>
+#include <processor/types.h>
+#include <machine/Machine.h>
+#include <machine/Device.h>
+#include <machine/Bus.h>
+#include <processor/IoPort.h>
+#include <utilities/utility.h>
+
+/** Architecture-independent interface to a PCI bus */
+class PciBus
+{
+    public:
+        PciBus()
+        {
+        }
+        virtual ~PciBus()
+        {
+        }
+
+        static PciBus &instance()
+        {
+            return m_Instance;
+        }
+
+        /**
+         * Initialises the object for use.
+         */
+        void initialise();
+
+        /**
+         * Reads from the configuration space
+         * \param pDev the device to read configuration space for
+         * \param offset the offset into the configuration space to read
+         */
+        uint32_t readConfigSpace(Device *pDev, uint8_t offset);
+
+        /**
+         * Writes to the configuration space
+         * \param pDev the device to write configuration space for
+         * \param offset the offset into the configuration space to write
+         * \param data the data to write
+         */
+        void writeConfigSpace(Device *pDev, uint8_t offset, uint32_t data);
+
+    private:
+        static PciBus m_Instance;
+};
+
+#endif
---
+++ src/modules/drivers/common/pci-common/x86/pci.cc
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2008 James Molloy, Jörg Pfähler, Matthew Iselin
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include <Log.h>
+#include <Module.h>
+#include <processor/types.h>
+#include <machine/Machine.h>
+#include <machine/Device.h>
+#include <machine/Bus.h>
+#include <processor/IoPort.h>
+#include <utilities/utility.h>
+#include "../pci.h"
+
+#define CONFIG_ADDRESS 0
+#define CONFIG_DATA    4
+
+#define MAX_BUS 4
+
+IoPort configSpace("PCI config space");
+
+union ConfigAddress {
+  struct
+  {
+    uint32_t always0  : 2;
+    uint32_t offset   : 6;
+    uint32_t function : 3;
+    uint32_t device   : 5;
+    uint32_t bus      : 8;
+    uint32_t reserved : 7;
+    uint32_t enable   : 1;
+  } __attribute__((packed));
+  uint32_t raw;
+};
+
+PciBus PciBus::m_Instance;
+
+void PciBus::initialise()
+{
+    if (!configSpace.allocate(0xCF8, 8))
+    {
+        ERROR("PCI: Config space - unable to allocate IO port!");
+        return;
+    }
+
+    configSpace.write32(0x80000000, CONFIG_ADDRESS);
+    if (configSpace.read32(CONFIG_ADDRESS) != 0x80000000)
+    {
+        ERROR("PCI: Controller not detected.");
+        return;
+    }
+}
+
+uint32_t PciBus::readConfigSpace(Device *pDev, uint8_t offset)
+{
+    ConfigAddress addr;
+    addr.raw = 0;
+    addr.offset = offset;
+    addr.function = pDev->getPciFunctionNumber();
+    addr.device = pDev->getPciDeviceId();
+    addr.bus = pDev->getPciBusPosition();
+    addr.enable = 1;
+
+    configSpace.write32(addr.raw, CONFIG_ADDRESS);
+    return configSpace.read32(CONFIG_DATA);
+}
+
+void PciBus::writeConfigSpace(Device *pDev, uint8_t offset, uint32_t data)
+{
+    ConfigAddress addr;
+    addr.raw = 0;
+    addr.offset = offset;
+    addr.function = pDev->getPciFunctionNumber();
+    addr.device = pDev->getPciDeviceId();
+    addr.bus = pDev->getPciBusPosition();
+    addr.enable = 1;
+
+    configSpace.write32(addr.raw, CONFIG_ADDRESS);
+    configSpace.write32(data, CONFIG_DATA);
+}
