a6937cc70b64deb5649725593acc48d42f97dfc3
Matthew Iselin <matthew@theiselins.net>
2009-12-14 14:20:27 +1000

    Pad out class Object to add the general planned interface. Add class Test to aid in developing the method for performing native API system calls.

--- src/subsys/native/include/Object.h
+++ src/subsys/native/include/Object.h
@@ -14,6 +14,11 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
+#ifndef _PEDIGREE_OBJECT_H
+#define _PEDIGREE_OBJECT_H
+
+#include <processor/types.h>
+
 /** Object is merely the base class for all native subsystem objects. */
 class Object
 {
@@ -24,4 +29,39 @@ class Object
         virtual ~Object()
         {
         }
+
+        /** \brief Serialises all relevant class data into a buffer.
+         *
+         * serialise is called when a request needs to be sent to the kernel
+         * to perform an action. The class provides an implementation which
+         * fills the buffer with class data that is deemed relevant. The
+         * buffer is then transmitted to the kernel where it is passed to a
+         * kernel-side object and unseralised.
+         *
+         * @param buffer must be filled with either 0 (serialisation failed)
+         *               or the address of the serialised buffer
+         * @param length must contain the length of the allocated buffer, in
+         *               bytes
+         */
+        virtual void serialise(uintptr_t *buffer, size_t &length)
+        {
+        }
+
+        /** \brief Unserialises all relevant class data from a buffer.
+         *
+         * unserialise is called when a buffer needs to be converted into
+         * class data. These buffers are filled by serialise calls.
+         *
+         * Userspace applications generally do not need to implement
+         * unserialise, as most unserialisation is done on the kernel's side
+         * of the native API.
+         *
+         * @param buffer address of the buffer to unserialise
+         * @param length the length of the buffer
+         */
+        virtual void unserialise(uintptr_t buffer, size_t &length)
+        {
+        }
 };
+
+#endif
