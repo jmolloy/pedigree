318133632d5e97072f90428b905f71232c701f76
Matthew Iselin <matthew@theiselins.net>
2009-12-16 18:47:33 +1000

    Added proper options to modlist (-a, -l, -u)

--- src/user/applications/modlist/main.c
+++ src/user/applications/modlist/main.c
@@ -1,3 +1,4 @@
+#include <unistd.h>
 #include <dirent.h>
 #include <libgen.h>
 #include <stdio.h>
@@ -14,6 +15,60 @@ extern int pedigree_module_is_loaded(char *name);
 ///       unloaded modules.
 int main(int argc, char **argv)
 {
+    /*extern char *optarg;
+    extern int optind, optopt;*/
+
+    int listAll = 0, listLoaded = 0, listUnloaded = 0, c = 0, err = 0;
+    while((c = getopt(argc, argv, "aluh")) != -1)
+    {
+        switch(c)
+        {
+            case 'a':
+                if(listLoaded || listUnloaded)
+                    err = 1;
+                else
+                    listAll = 1;
+                break;
+            case 'l':
+                if(listAll || listUnloaded)
+                    err = 1;
+                else
+                    listLoaded = 1;
+                break;
+            case 'u':
+                if(listAll || listLoaded)
+                    err = 1;
+                else
+                    listUnloaded = 1;
+                break;
+            case 'h':
+                fprintf(stderr, "usage: modlist [-a] [-l] [-u]\n");
+                fprintf(stderr, "-a%-16slist all modules and show their status\n", "");
+                fprintf(stderr, "-l%-16slist all already-loaded modules\n", "");
+                fprintf(stderr, "-u%-16slist all modules that are not loaded\n", "");
+                fprintf(stderr, "-h%-16sshow this usage information\n", "");
+                fprintf(stderr, "\n");
+                fprintf(stderr, "modlist without arguments defaults to listing all modules with their status\n");
+                err = 3;
+                break;
+            default:
+                fprintf(stderr, "Unrecognized option: -%c\n", optopt);
+                err = 2;
+        }
+    }
+
+    if(!listAll && !listLoaded && !listUnloaded)
+        listAll = 1;
+
+    if(err)
+    {
+        if(err == 1)
+            fprintf(stderr, "Only one action (-a, -l, or -u) can be specified\n");
+        else if(err == 3)
+            return 0;
+        return err;
+    }
+
     DIR *dp = opendir(MODULE_DIR);
     if(!dp)
     {
@@ -30,9 +85,25 @@ int main(int argc, char **argv)
         char *suffix = lastPeriod + 1;
         if(lastPeriod && !stricmp(suffix, "o"))
         {
-            *(suffix - 1) = '\0';
-            printf("%-32s%s", filename, pedigree_module_is_loaded(filename) ? "[loaded]" : "");
-            printf("\n");
+            *lastPeriod = '\0';
+            if(listAll)
+            {
+                printf("%-32s%s\n", filename, pedigree_module_is_loaded(filename) ? "[loaded]" : "");
+            }
+            else if(listLoaded)
+            {
+                if(pedigree_module_is_loaded(filename))
+                {
+                    printf("%s\n", filename);
+                }
+            }
+            else if(listUnloaded)
+            {
+                if(!pedigree_module_is_loaded(filename))
+                {
+                    printf("%s\n", filename);
+                }
+            }
         }
     }
     closedir(dp);
