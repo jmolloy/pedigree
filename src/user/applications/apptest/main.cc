202c428287aba0caaee48307987e13ee77104a4e
Matthew Iselin <matthew@theiselins.net>
2010-01-05 20:19:15 +1000

    Remove the native subsystem from the x64 build for now

--- src/user/applications/apptest/main.cc
+++ src/user/applications/apptest/main.cc
@@ -1,3 +1,5 @@
+#ifdef X86
+
 #include <types.h>
 #include <test.h>
 
@@ -7,3 +9,17 @@ int main(int argc, char **argv)
     whee.writeLog("Hello world", 12);
     return 0;
 }
+
+#else
+
+// Architectures which don't support the native interface will pretend :)
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    printf("Boring old printf works... No native API on this architecture yet");
+    return 0;
+}
+
+#endif
