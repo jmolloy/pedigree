749dd57bcb5c5baf03315b0956b5bb836f26f849
Matthew Iselin <matthew@theiselins.net>
2010-04-19 20:07:45 +1000

    Added a "debug allocator" which is pretty much placement new. It adds an unmapped page after every allocation to detect buffer overflows. Horribly inefficient, only turn on if you really have to!

--- src/system/kernel/core/lib/cppsupport.cc
+++ src/system/kernel/core/lib/cppsupport.cc
@@ -19,9 +19,19 @@
 #include "cppsupport.h"
 #include <panic.h>
 #include <processor/Processor.h>
+#include <processor/PhysicalMemoryManager.h>
+#include <processor/VirtualAddressSpace.h>
 #include <Log.h>
 
 #include "SlamAllocator.h"
+
+/// The debug allocator cannot free memory and is designed to find overflows and
+/// underflows only.
+// #define USE_DEBUG_ALLOCATOR
+
+/// If the debug allocator is enabled, this switches it into underflow detection
+/// mode.
+#define DEBUG_ALLOCATOR_CHECK_UNDERFLOWS
 
 // Required for G++ to link static init/destructors.
 void *__dso_handle;
@@ -29,6 +39,11 @@ void *__dso_handle;
 // Defined in the linker.
 uintptr_t start_ctors;
 uintptr_t end_ctors;
+
+#ifdef USE_DEBUG_ALLOCATOR
+Spinlock allocLock;
+uintptr_t heapBase = 0x60000000; // 0xC0000000;
+#endif
 
 /// Calls the constructors for all global objects.
 /// Call this before using any global objects.
@@ -69,14 +84,15 @@ extern "C" void __cxa_guard_release()
 
 extern "C" void *malloc(size_t sz)
 {
-    return reinterpret_cast<void *>(SlamAllocator::instance().allocate(sz));
+    return reinterpret_cast<void *>(new uint8_t[sz]); //SlamAllocator::instance().allocate(sz));
 }
 
 extern "C" void free(void *p)
 {
     if (p == 0)
         return;
-    SlamAllocator::instance().free(reinterpret_cast<uintptr_t>(p));
+    //SlamAllocator::instance().free(reinterpret_cast<uintptr_t>(p));
+    delete reinterpret_cast<uint8_t*>(p);
 }
 
 extern "C" void *realloc(void *p, size_t sz)
@@ -98,20 +114,102 @@ extern "C" void *realloc(void *p, size_t sz)
 
 void *operator new (size_t size) throw()
 {
-#if defined(X86_COMMON) || defined(MIPS_COMMON) || defined(PPC_COMMON)
-  void *ret = reinterpret_cast<void *>(SlamAllocator::instance().allocate(size));
-  return ret;
+#ifdef USE_DEBUG_ALLOCATOR
+
+    /// \todo underflow flag
+    
+    // Full size of the region to allocate
+    size_t nPages = ((size / 0x1000) + 1);
+    size_t realSize = nPages * 0x1000;
+    
+    allocLock.acquire();
+    
+    // Find the base of the unmapped region (overflow check)
+    uintptr_t currBase = heapBase;
+    uintptr_t overflowBase = currBase + realSize;
+    
+    // Update the base of the heap
+    heapBase = overflowBase + 0x1000;
+    
+    allocLock.release();
+    
+    // We return a block just before the unmapped region
+    uintptr_t ret = overflowBase - size;
+    
+    // Map in the pages we actually want to access
+    VirtualAddressSpace &va = Processor::information().getVirtualAddressSpace();
+    for(size_t i = 0; i < nPages; i++)
+    {
+        physical_uintptr_t page = PhysicalMemoryManager::instance().allocatePage();
+        // NOTICE("Mapping " << (currBase + (i * 0x1000)) << " to " << page);
+        bool success = va.map(
+                page,
+                reinterpret_cast<void*>(currBase + (i * 0x1000)),
+                VirtualAddressSpace::KernelMode | VirtualAddressSpace::Write
+        );
+        if(!success)
+            FATAL("Debug allocator - mapping failed!");
+    }
+    
+    // NOTICE("debug allocator returning " << ret);
+    
+    return reinterpret_cast<void*>(ret);
+
+#elif defined(X86_COMMON) || defined(MIPS_COMMON) || defined(PPC_COMMON)
+    void *ret = reinterpret_cast<void *>(SlamAllocator::instance().allocate(size));
+    return ret;
 #else
-  return 0;
+    return 0;
 #endif
 }
 void *operator new[] (size_t size) throw()
 {
-#if defined(X86_COMMON) || defined(MIPS_COMMON) || defined(PPC_COMMON)
-  void *ret = reinterpret_cast<void *>(SlamAllocator::instance().allocate(size));
-  return ret;
+#ifdef USE_DEBUG_ALLOCATOR
+    
+    /// \todo underflow flag
+    
+    // Full size of the region to allocate
+    size_t nPages = ((size / 0x1000) + 1);
+    size_t realSize = nPages * 0x1000;
+    
+    allocLock.acquire();
+    
+    // Find the base of the unmapped region (overflow check)
+    uintptr_t currBase = heapBase;
+    uintptr_t overflowBase = currBase + realSize;
+    
+    // Update the base of the heap
+    heapBase = overflowBase + 0x1000;
+    
+    allocLock.release();
+    
+    // We return a block just before the unmapped region
+    uintptr_t ret = overflowBase - size;
+    
+    // Map in the pages we actually want to access
+    VirtualAddressSpace &va = Processor::information().getVirtualAddressSpace();
+    for(size_t i = 0; i < nPages; i++)
+    {
+        physical_uintptr_t page = PhysicalMemoryManager::instance().allocatePage();
+        // NOTICE("Mapping " << (currBase + (i * 0x1000)) << " to " << page);
+        bool success = va.map(
+                page,
+                reinterpret_cast<void*>(currBase + (i * 0x1000)),
+                VirtualAddressSpace::KernelMode | VirtualAddressSpace::Write
+        );
+        if(!success)
+            FATAL("Debug allocator - mapping failed!");
+    }
+    
+    // NOTICE("debug allocator returning " << ret);
+    
+    return reinterpret_cast<void*>(ret);
+
+#elif defined(X86_COMMON) || defined(MIPS_COMMON) || defined(PPC_COMMON)
+    void *ret = reinterpret_cast<void *>(SlamAllocator::instance().allocate(size));
+    return ret;
 #else
-  return 0;
+    return 0;
 #endif
 }
 void *operator new (size_t size, void* memory) throw()
@@ -124,6 +222,10 @@ void *operator new[] (size_t size, void* memory) throw()
 }
 void operator delete (void * p)
 {
+#ifdef USE_DEBUG_ALLOCATOR
+    return;
+#endif
+
 #if defined(X86_COMMON) || defined(MIPS_COMMON) || defined(PPC_COMMON)
     if (p == 0) return;
     SlamAllocator::instance().free(reinterpret_cast<uintptr_t>(p));
@@ -131,6 +233,10 @@ void operator delete (void * p)
 }
 void operator delete[] (void * p)
 {
+#ifdef USE_DEBUG_ALLOCATOR
+    return;
+#endif
+
 #if defined(X86_COMMON) || defined(MIPS_COMMON) || defined(PPC_COMMON)
     if (p == 0) return;
     SlamAllocator::instance().free(reinterpret_cast<uintptr_t>(p));
