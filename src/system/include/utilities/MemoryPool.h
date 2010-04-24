644549754f5ff95667d87377569887d45ce48b39
Matthew Iselin <matthew@theiselins.net>
2010-04-24 10:53:49 +1000

    Implemented MemoryPool. Compile tested, will test it out when I rework the network stack to use it.

--- src/system/include/utilities/MemoryPool.h
+++ src/system/include/utilities/MemoryPool.h
@@ -16,4 +16,65 @@
 #ifndef _UTILITY_MEMORY_POOL
 #define _UTILITY_MEMORY_POOL
 
+#include <processor/types.h>
+#include <process/Semaphore.h>
+#include <processor/MemoryRegion.h>
+#include <utilities/ExtensibleBitmap.h>
+#include <processor/PhysicalMemoryManager.h>
+
+/** MemoryPool - a class which encapsulates a pool of memory with constant
+  * sized buffers that can be allocated around the kernel. Intended to be
+  * used instead of the heap for areas where similar sized buffers are
+  * regularly allocated, such as networking code. */
+class MemoryPool
+{
+    public:
+        MemoryPool();
+        virtual ~MemoryPool();
+
+        /// Initialises the pool, preparing it for use
+        /// @param poolSize Number of pages in the pool.
+        /// @param bufferSize Size of each buffer. Will be rounded to the
+        ///                   next power of two.
+        bool initialise(size_t poolSize, size_t bufferSize = 1024);
+        
+        /// Call if you aren't certain that the object has been initialised yet
+        inline bool initialised()
+        {
+            return m_bInitialised;
+        }
+
+        /// Allocates a buffer from the pool. Will block if no buffers are
+        /// available yet.
+        uintptr_t allocate();
+
+        /// Allocates a buffer from the pool. If no buffers are available, this
+        /// function will return strawight away.
+        /// @return Zero if a buffer couldn't be allocated.
+        uintptr_t allocateNow();
+
+        /// Frees an allocated buffer, allowing it to be used elsewhere
+        void free(uintptr_t buffer);
+
+    private:
+        /// This Semaphore tracks the number of buffers allocated, and allows
+        /// blocking when the buffers run out.
+        Semaphore m_BlockSemaphore;
+
+        /// Size of each buffer in this pool
+        size_t m_BufferSize;
+
+        /// MemoryRegion describing the actual pool of memory
+        MemoryRegion m_Pool;
+        
+        /// Has this instance been initialised yet?
+        bool m_bInitialised;
+
+        /// Allocation bitmap
+        ExtensibleBitmap m_AllocBitmap;
+
+        /// Allocation doer
+        uintptr_t allocateDoer();
+};
+
 #endif
--- src/system/kernel/utilities/MemoryPool.cc
+++ src/system/kernel/utilities/MemoryPool.cc
@@ -13,4 +13,104 @@
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
+#include <processor/Processor.h>
 #include <utilities/MemoryPool.h>
+#include <processor/VirtualAddressSpace.h>
+#include <Log.h>
+
+MemoryPool::MemoryPool() :
+    m_BlockSemaphore(0), m_BufferSize(1024), m_Pool("memory-pool"),
+        m_bInitialised(false), m_AllocBitmap()
+{
+}
+
+MemoryPool::~MemoryPool()
+{
+    // Free all the buffers
+    m_bInitialised = false;
+    while(!m_BlockSemaphore.getValue())
+        m_BlockSemaphore.release();
+}
+
+bool MemoryPool::initialise(size_t poolSize, size_t bufferSize)
+{
+    if(m_bInitialised)
+        return true;
+
+    if(!poolSize || !bufferSize || (bufferSize > poolSize))
+        return false;
+
+    // Find the next power of two for bufferSize, if it isn't already one
+    if(!(bufferSize & (bufferSize - 1)))
+    {
+        size_t powerOf2 = 1;
+        size_t lg2 = 0;
+        while(powerOf2 < bufferSize)
+        {
+            powerOf2 <<= 1;
+            lg2 ++;
+        }
+        bufferSize = powerOf2;
+    }
+
+    m_BufferSize = bufferSize;
+
+    m_bInitialised = PhysicalMemoryManager::instance().allocateRegion(
+        m_Pool,
+        poolSize,
+        0,
+        VirtualAddressSpace::Write
+    );
+    if(!m_bInitialised)
+        return false;
+
+    size_t nBuffers = (poolSize * 0x1000) / bufferSize;
+    m_BlockSemaphore.release(nBuffers);
+
+    return true;
+}
+
+uintptr_t MemoryPool::allocate()
+{
+    /// \bug Race if another allocate() call occurs between the acquire and the doer
+    m_BlockSemaphore.acquire();
+    return allocateDoer();
+}
+
+uintptr_t MemoryPool::allocateNow()
+{
+    if(m_BlockSemaphore.tryAcquire())
+        return allocateDoer();
+    else
+        return 0;
+}
+
+uintptr_t MemoryPool::allocateDoer()
+{
+    // Find a free buffer
+    size_t poolSize = m_Pool.size();
+    size_t nBuffers = poolSize / m_BufferSize;
+    uintptr_t poolBase = reinterpret_cast<uintptr_t>(m_Pool.virtualAddress());
+    for(size_t n = 0; n < nBuffers; n++)
+    {
+        if(!m_AllocBitmap.test(n))
+        {
+            m_AllocBitmap.set(n);
+            return poolBase + (n * 0x1000);
+        }
+    }
+
+    FATAL("allocateDoer failed - known race condition");
+    return 0;
+}
+
+void MemoryPool::free(uintptr_t buffer)
+{
+    if(!m_bInitialised)
+        return;
+
+    size_t n = (buffer - reinterpret_cast<uintptr_t>(m_Pool.virtualAddress())) / m_BufferSize;
+    m_AllocBitmap.clear(n);
+
+    m_BlockSemaphore.release();
+}
