072df1b8d6b0070cb19f035ad645d9f369eb6b65
Matthew Iselin <matthew@theiselins.net>
2010-05-24 17:10:31 +1000

    Updated the Visual Studio pipe handler script to use the correct path for my system

--- scripts/visualStudioMakePipe.sh
+++ scripts/visualStudioMakePipe.sh
@@ -5,7 +5,7 @@ do
 case $line in
   src/*)
 	# turn into a windows path
-	CYGPATH=`echo /cygdrive/f/pedigree/pedigree/$line | sed -r -e "s/([^:]+)(.+)/\1/"`
+	CYGPATH=`echo /cygdrive/c/files/projects/pedigree/pedigree/$line | sed -r -e "s/([^:]+)(.+)/\1/"`
 	WINPATH=`cygpath -aw $CYGPATH`
 	WINPATH=`echo $WINPATH | awk -F"\\\\" '{$1=$1}1' OFS="\\\\\\\/"`
 	FINAL=`echo $line | sed -r -e "s/(.+):(.[0-9]+):/"$WINPATH"(\2) : /"`
