f77743cb20db195e70931e29cbcc09a8eee9f8b8
Matthew Iselin <matthew@theiselins.net>
2010-06-11 14:10:03 +1000

    Fixed copyright header for files I modified at work

--- src/system/kernel/core/processor/armv7/VirtualAddressSpace.cc
+++ src/system/kernel/core/processor/armv7/VirtualAddressSpace.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008 James Molloy, Jörg Pfähler, Matthew Iselin
+ * Copyright (c) 2010 Matthew Iselin, Heights College
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
