27b8107c315fb02daedea3a8295cdeed08a46549
Matthew Iselin <matthew@theiselins.net>
2010-07-06 10:47:36 +1000

    ARM: Added comments to the ULPI stuff (this should be in an omap3 directory or something...). Also added to code to ensure the USB port has power.

--- src/modules/drivers/arm/usb-glue/UsbUlpi.cc
+++ src/modules/drivers/arm/usb-glue/UsbUlpi.cc
@@ -17,12 +17,16 @@
 #include "UsbUlpi.h"
 #include <processor/PhysicalMemoryManager.h>
 #include <processor/VirtualAddressSpace.h>
+#include <process/Semaphore.h>
 #include <Log.h>
 
 /// \todo super specific to a machine
 #include <Prcm.h>
+#include <I2C.h>
 
 UsbUlpi UsbUlpi::m_Instance;
+
+#define delay(n) do{Semaphore semWAIT(0);semWAIT.acquire(1, 0, n*1000);}while(0)
 
 void UsbUlpi::initialise()
 {
@@ -49,21 +53,25 @@ void UsbUlpi::initialise()
     }
 
     volatile uint32_t *pctl_base = reinterpret_cast<volatile uint32_t*>(m_MemRegionPCtl.virtualAddress());
-    pctl_base[(0x400) / 4] = 1;
-    pctl_base[(0x400 + 0x10) / 4] = 1;
-    pctl_base[(0x400 + 0x20) / 4] = 0;
-    pctl_base[(0x400 + 0x4C) / 4] = 1;
+    pctl_base[(0x400 + 0x20) / 4] = 0; // USB HOST is active
+    pctl_base[(0x400 + 0x10) / 4] = 1; // Interface clock enabled
+    pctl_base[(0x400 + 0x4C) / 4] = 1; // Domain interface clock active
+    pctl_base[(0x400) / 4] = 3; // Both functional clocks enabled
 
-    Prcm::instance().SetIfaceClockCORE(3, 2, true);
-
-    volatile uint8_t *uhh_base = reinterpret_cast<volatile uint8_t*>(m_MemRegionUHH.virtualAddress());
+    volatile uint32_t *uhh_base = reinterpret_cast<volatile uint32_t*>(m_MemRegionUHH.virtualAddress());
     NOTICE("USB UHH: Revision " << Dec << ((uhh_base[0] >> 4) & 0xF) << "." << (uhh_base[0] & 0xF) << Hex << ".");
 
     // Reset the entire USB module
-    uhh_base[0x10] = 2;
-    while(!(uhh_base[0x14]));
+    uhh_base[0x10 / 4] = 2;
+    while(!(uhh_base[0x14 / 4])) delay(5);
 
     // Set up power management etc
-    uhh_base[0x10] = 8; // No idle
-    uhh_base[0x11] = 0x11; // No standby and clock is always on
+    uhh_base[0x10 / 4] = 0; // No idle
+    uhh_base[0x40 / 4] = 0x700; // Enable all three USB ports
+
+    // 0x33 = LEDA, LEDB ON
+    // LEDA = nEN_USB_PWR, drives power to the USB port
+    // LEDB = PMU STAT LED on the BeagleBoard
+    uint8_t buffer[2] = {0xEE, 0x33};
+    I2C::instance(0).transmit(0x4A, reinterpret_cast<uintptr_t>(buffer), 2);
 }
