f68e1dd6e05c60a0dbf8eabbe48be16c459d6f87
Matthew Iselin <matthew@theiselins.net>
2010-07-08 16:22:06 +1000

    ARM: ULPI updates. At the moment this hangs waiting for the USBTLL module to come out of idle mode - even though its iclk and fclk are both turned on. Still debugging...

--- src/modules/drivers/arm/usb-glue/UsbUlpi.cc
+++ src/modules/drivers/arm/usb-glue/UsbUlpi.cc
@@ -23,10 +23,56 @@
 /// \todo super specific to a machine
 #include <Prcm.h>
 #include <I2C.h>
+#include <Gpio.h>
 
 UsbUlpi UsbUlpi::m_Instance;
 
 #define delay(n) do{Semaphore semWAIT(0);semWAIT.acquire(1, 0, n*1000);}while(0)
+
+bool usbWriteTwl4030(uint8_t addr, uint8_t data)
+{
+    return I2C::instance(0).write(0x48, addr, data);
+}
+
+uint8_t usbReadTwl4030(uint8_t addr)
+{
+    return I2C::instance(0).read(0x48, addr);
+}
+
+/// usbSetBits: In the ULPI register bank, a single RW register is followed
+///             by an _SET and _CLR register. Writing a 1 to any bit in these
+///             registers will set or clear a bit in the main register.
+bool usbSetBits(uint8_t reg, uint8_t bits)
+{
+    return usbWriteTwl4030(reg + 1, bits);
+}
+
+/// usbClearBits: see usbSetBits
+bool usbClearBits(uint8_t reg, uint8_t bits)
+{
+    return usbWriteTwl4030(reg + 2, bits);
+}
+
+/// Enable access to the USB PHY registers over the I2C bus
+void enablePhyAccess(bool which)
+{
+    uint8_t clock = usbReadTwl4030(0xFE);
+    if(clock)
+    {
+        if(which)
+        {
+            clock |= 1; // Request DPLL clock
+            usbWriteTwl4030(0xFE, clock);
+            while(!(usbReadTwl4030(0xFF) & 1))
+                delay(10);
+        }
+        else
+        {
+            clock &= ~1;
+            usbWriteTwl4030(0xFE, clock);
+        }
+    }
+}
 
 void UsbUlpi::initialise()
 {
@@ -42,21 +88,103 @@ void UsbUlpi::initialise()
         return;
     }
     if(!PhysicalMemoryManager::instance().allocateRegion(
+                            m_MemRegionTLL,
+                            1,
+                            PhysicalMemoryManager::continuous | PhysicalMemoryManager::force,
+                            VirtualAddressSpace::KernelMode | VirtualAddressSpace::Write,
+                            0x48062000))
+    {
+        ERROR("USB TLL: Couldn't get a memory region!");
+        return;
+    }
+    if(!PhysicalMemoryManager::instance().allocateRegion(
                             m_MemRegionPCtl,
                             2,
                             PhysicalMemoryManager::continuous | PhysicalMemoryManager::force,
                             VirtualAddressSpace::KernelMode | VirtualAddressSpace::Write,
                             0x48005000))
     {
-        ERROR("USB TLL: Couldn't get a memory region!");
+        ERROR("USB Power Control: Couldn't get a memory region!");
         return;
     }
 
+    NOTICE("Initialising USB Controller...");
+
+    // 0x33 = LEDA, LEDB ON
+    // LEDA = nEN_USB_PWR, drives power to the USB port
+    // LEDB = PMU STAT LED on the BeagleBoard
+    uint8_t buffer[2] = {0xEE, 0x33};
+    I2C::instance(0).transmit(0x4A, reinterpret_cast<uintptr_t>(buffer), 2);
+
+    // Enable the power configuration registers, so we can enable the voltage
+    // regulators for USB.
+    I2C::instance(0).write(0x4b, 0x44, 0xC0); // PROTECT_KEY
+    I2C::instance(0).write(0x4b, 0x44, 0x0C);
+
+    // VUSB3V1 = active
+    I2C::instance(0).write(0x4b, VUSB_DEDICATED2, 0);
+
+    // VUSB3V1 input = VBAT
+    I2C::instance(0).write(0x4b, VUSB_DEDICATED1, 0x14);
+    
+    // Turn on the 3.1 volt regulator
+    I2C::instance(0).write(0x4b, VUSB3V1_DEV_GRP, 0x20);
+    I2C::instance(0).write(0x4b, VUSB3V1_TYPE, 0);
+
+    // Turn on the 1.5 volt regulator
+    I2C::instance(0).write(0x4b, VUSB1V5_DEV_GRP, 0x20);
+    I2C::instance(0).write(0x4b, VUSB1V5_TYPE, 0);
+
+    // Turn on the 1.8 volt regulator
+    I2C::instance(0).write(0x4b, VUSB1V8_DEV_GRP, 0x20);
+    I2C::instance(0).write(0x4b, VUSB1V8_TYPE, 0);
+
+    // Disable access to the power configuration registers... we're done
+    I2C::instance(0).write(0x4b, 0x44, 0);
+
+    // Enable the USB PHY for all three ports
+    uint8_t power = usbReadTwl4030(0xFD);
+    power &= ~1;
+    usbWriteTwl4030(0xFD, power);
+    usbWriteTwl4030(0xFE, usbReadTwl4030(0xFE) | (1 << 2) | (1 << 1));
+
+    // Setup ULPI
+    enablePhyAccess(true);
+    usbClearBits(InterfaceControl, 1 << 2); // Disable carkit mode
+
+    // Set the controller as active
+    usbClearBits(OtgControl, 2); // Disable the D+ pull-down resistor
+    usbSetBits(0xAC, 1 << 5); // Enable OTG
+    usbSetBits(FunctionControl, 4); // FS termination enabled
+    usbClearBits(FunctionControl, 0x1B); // Enable the HS transceiver
+    enablePhyAccess(false);
+
+    // Configure the L3 and L4 clocks
+    Prcm::instance().SelectClockCORE(0, Prcm::L3_CLK_DIV2);
+    Prcm::instance().SelectClockCORE(2, Prcm::L3_CLK_DIV2);
+
     volatile uint32_t *pctl_base = reinterpret_cast<volatile uint32_t*>(m_MemRegionPCtl.virtualAddress());
-    pctl_base[(0x400 + 0x20) / 4] = 0; // USB HOST is active
-    pctl_base[(0x400 + 0x10) / 4] = 1; // Interface clock enabled
-    pctl_base[(0x400 + 0x4C) / 4] = 1; // Domain interface clock active
     pctl_base[(0x400) / 4] = 3; // Both functional clocks enabled
+    pctl_base[(0x400 + 0x10) / 4] = 1; // Interface clock enabled
+    pctl_base[(0x400 + 0x30) / 4] = 0; // Disable automatic control of the interface clock enabled
+
+    volatile uint32_t *tll_base = reinterpret_cast<volatile uint32_t*>(m_MemRegionTLL.virtualAddress());
+    volatile uint8_t *ulpi_base = reinterpret_cast<volatile uint8_t*>(reinterpret_cast<uintptr_t>(m_MemRegionTLL.virtualAddress()) + 0x800);
+
+    // Perform a PHY reset
+    Gpio::instance().enableoutput(147);
+    Gpio::instance().clearpin(147);
+    delay(10); // Hold reset long enough
+
+    // Enable the TLL clocks
+    Prcm::instance().SetFuncClockCORE(3, 2, true);
+    Prcm::instance().SetIfaceClockCORE(3, 2, true);
+
+    // Wait for the TLL to come online - we must not access any TLL registers
+    // while it's still powering up
+    Prcm::instance().WaitCoreIdleStatus(3, 2, true);
+
+    uint32_t rev = tll_base[0];
 
     volatile uint32_t *uhh_base = reinterpret_cast<volatile uint32_t*>(m_MemRegionUHH.virtualAddress());
     NOTICE("USB UHH: Revision " << Dec << ((uhh_base[0] >> 4) & 0xF) << "." << (uhh_base[0] & 0xF) << Hex << ".");
@@ -65,13 +193,14 @@ void UsbUlpi::initialise()
     uhh_base[0x10 / 4] = 2;
     while(!(uhh_base[0x14 / 4])) delay(5);
 
-    // Set up power management etc
-    uhh_base[0x10 / 4] = 0; // No idle
-    uhh_base[0x40 / 4] = 0x700; // Enable all three USB ports
+    // Set up idle mode
+    uhh_base[0x10 / 4] = (1 << 2) | (1 << 3) | (1 << 8) | (1 << 12); // No idle
 
-    // 0x33 = LEDA, LEDB ON
-    // LEDA = nEN_USB_PWR, drives power to the USB port
-    // LEDB = PMU STAT LED on the BeagleBoard
-    uint8_t buffer[2] = {0xEE, 0x33};
-    I2C::instance(0).transmit(0x4A, reinterpret_cast<uintptr_t>(buffer), 2);
+    // Configure ULPI bypass configuration
+    uhh_base[0x40 / 4] = (1 << 2) | (1 << 3) | (1 << 4);
+    uhh_base[0x40 / 4] = 0x700; // Connect all three ports, ULPI not UTMI
+
+    // Restore the PHY
+    delay(10);
+    Gpio::instance().drivepin(147);
 }
--- src/modules/drivers/arm/usb-glue/UsbUlpi.h
+++ src/modules/drivers/arm/usb-glue/UsbUlpi.h
@@ -22,7 +22,7 @@
 class UsbUlpi
 {
     public:
-        UsbUlpi() : m_MemRegionUHH("USB-UHH_CONFIG"), m_MemRegionPCtl("USB-PCTL")
+        UsbUlpi() : m_MemRegionUHH("USB-UHH_CONFIG"), m_MemRegionTLL("USB-TLL"), m_MemRegionPCtl("USB-PCTL")
         {
         }
         virtual ~UsbUlpi()
@@ -40,7 +40,56 @@ class UsbUlpi
 
         static UsbUlpi m_Instance;
 
+        enum UlpiRegisters
+        {
+            VendorIdLo          = 0,
+            VendorIdHi          = 1,
+            ProductIdLo         = 2,
+            ProductIdHi         = 3,
+            FunctionControl     = 4,
+            InterfaceControl    = 7,
+            OtgControl          = 0xA,
+            UsbIntEnableRise    = 0xD,
+            UsbIntEnableFall    = 0x10,
+            UsbIntStatus        = 0x13,
+            UsbIntLatch         = 0x14,
+            Debug               = 0x15,
+            Scratch             = 0x16,
+            ExtendedSetAccess   = 0x2F,
+            UtmiVControlEnable  = 0x30,
+            UtmiVControlStatus  = 0x33,
+            UtmiVControlLatch   = 0x34,
+            UtmiVStatus         = 0x35,
+            UtmiIntLatchNoClr   = 0x38,
+            VendorIntEnable     = 0x3B,
+            VendorIntStatus     = 0x3E,
+            VendorIntLatch      = 0x3F
+        };
+
+        enum PowerManagement
+        {
+            VUSB1V5_DEV_GRP     = 0xCC,
+            VUSB1V5_TYPE        = 0xCD,
+            VUSB1V5_REMAP       = 0xCE,
+
+            VUSB1V8_DEV_GRP     = 0xCF,
+            VUSB1V8_TYPE        = 0xD0,
+            VUSB1V8_REMAP       = 0xD1,
+
+            VUSB3V1_DEV_GRP     = 0xD2,
+            VUSB3V1_TYPE        = 0xD3,
+            VUSB3V1_REMAP       = 0xD4,
+
+            VUSBCP_DEV_GRP      = 0xD5,
+            VUSBCP_TYPE         = 0xD6,
+            VUSBCP_REMAP        = 0xD7,
+
+            VUSB_DEDICATED1     = 0xD8,
+            VUSB_DEDICATED2     = 0xD9,
+        };
+
         MemoryRegion m_MemRegionUHH;
+        MemoryRegion m_MemRegionTLL;
         MemoryRegion m_MemRegionPCtl;
 };
 
