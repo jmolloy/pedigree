3b6c8a28f265f129665f680913ae69e008869818
Matthew Iselin <matthew@theiselins.net>
2010-08-04 21:28:38 +1000

    Stubbed DM9601 base functions - including all USB vendor functions that'll be used for proper driver code.

--- src/modules/drivers/common/dm9601/Dm9601.cc
+++ src/modules/drivers/common/dm9601/Dm9601.cc
@@ -14,4 +14,83 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 #include "Dm9601.h"
+#include <Log.h>
+
+bool Dm9601::send(size_t nBytes, uintptr_t buffer)
+{
+    return false;
+}
+
+bool Dm9601::setStationInfo(StationInfo info)
+{
+    // Free the old DNS server list, if there is one
+    if(m_StationInfo.dnsServers)
+        delete [] m_StationInfo.dnsServers;
+
+    // MAC isn't changeable, so set it all manually
+    m_StationInfo.ipv4 = info.ipv4;
+    NOTICE("DM9601: Setting ipv4, " << info.ipv4.toString() << ", " << m_StationInfo.ipv4.toString() << "...");
+    m_StationInfo.ipv6 = info.ipv6;
+
+    m_StationInfo.subnetMask = info.subnetMask;
+    NOTICE("DM9601: Setting subnet mask, " << info.subnetMask.toString() << ", " << m_StationInfo.subnetMask.toString() << "...");
+    m_StationInfo.gateway = info.gateway;
+    NOTICE("DM9601: Setting gateway, " << info.gateway.toString() << ", " << m_StationInfo.gateway.toString() << "...");
+
+    // Callers do not free their dnsServers memory
+    m_StationInfo.dnsServers = info.dnsServers;
+    m_StationInfo.nDnsServers = info.nDnsServers;
+    NOTICE("DM9601: Setting DNS servers [" << Dec << m_StationInfo.nDnsServers << Hex << " servers being set]...");
+
+    return true;
+}
+
+StationInfo Dm9601::getStationInfo()
+{
+    return m_StationInfo;
+}
+
+/// Reads data from a register into a buffer
+ssize_t Dm9601::readRegister(uint8_t reg, uintptr_t buffer, size_t nBytes)
+{
+    if(!buffer || (nBytes > 0xFF))
+        return -1;
+    return controlRequest(RequestType::Vendor | RequestDirection::In, ReadRegister, 0, reg, nBytes, buffer);
+}
+
+/// Writes data from a buffer to a register
+ssize_t Dm9601::writeRegister(uint8_t reg, uintptr_t buffer, size_t nBytes)
+{
+    if(!buffer || (nBytes > 0xFF))
+        return -1;
+    return controlRequest(RequestType::Vendor | RequestDirection::Out, WriteRegister, 0, reg, nBytes, buffer);
+}
+
+/// Writes a single 8-bit value to a register
+ssize_t Dm9601::writeRegister(uint8_t reg, uint8_t data)
+{
+    return controlRequest(RequestType::Vendor | RequestDirection::Out, WriteRegister1, data, reg);
+}
+
+/// Reads data from device memory into a buffer
+ssize_t Dm9601::readMemory(uint16_t offset, uintptr_t buffer, size_t nBytes)
+{
+    if(!buffer || (nBytes > 0xFF))
+        return -1;
+    return controlRequest(RequestType::Vendor | RequestDirection::In, ReadMemory, 0, offset, nBytes, buffer);
+}
+
+/// Writes data from a buffer into device memory
+ssize_t Dm9601::writeMemory(uint16_t offset, uintptr_t buffer, size_t nBytes)
+{
+    if(!buffer || (nBytes > 0xFF))
+        return -1;
+    return controlRequest(RequestType::Vendor | RequestDirection::Out, WriteMemory, 0, offset, nBytes, buffer);
+}
+
+/// Writes a single 8-bit value into device memory
+ssize_t Dm9601::writeMemory(uint16_t offset, uint8_t data)
+{
+    return controlRequest(RequestType::Vendor | RequestDirection::Out, WriteMemory1, data, offset);
+}
 
--- src/modules/drivers/common/dm9601/Dm9601.h
+++ src/modules/drivers/common/dm9601/Dm9601.h
@@ -13,4 +13,74 @@
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
+#ifndef DM9601_H
+#define DM9601_H
+
+#include <processor/types.h>
+#include <machine/Device.h>
+#include <machine/Network.h>
+#include <processor/IoBase.h>
+#include <processor/IoPort.h>
+#include <processor/MemoryRegion.h>
+#include <processor/PhysicalMemoryManager.h>
+#include <machine/IrqHandler.h>
+#include <process/Thread.h>
+#include <process/Semaphore.h>
+#include <utilities/List.h>
+#include <usb/UsbDevice.h>
+
+class Dm9601 : public UsbDevice, public Network
+{
+    public:
+    
+        Dm9601(UsbDevice *pDev);
+        
+        virtual ~Dm9601();
+
+        virtual void getName(String &str)
+        {
+            str = "DM9601";
+        }
+
+        virtual bool send(size_t nBytes, uintptr_t buffer);
+
+        virtual bool setStationInfo(StationInfo info);
+
+        virtual StationInfo getStationInfo();
+        
+    private:
+    
+        enum VendorRequests
+        {
+            ReadRegister    = 0,
+            WriteRegister   = 1,
+            ReadMemory      = 2,
+            WriteRegister1  = 3,
+            WriteMemory     = 5,
+            WriteMemory1    = 7,
+        };
+    
+        /// Reads data from a register into a buffer
+        ssize_t readRegister(uint8_t reg, uintptr_t buffer, size_t nBytes);
+        
+        /// Writes data from a buffer to a register
+        ssize_t writeRegister(uint8_t reg, uintptr_t buffer, size_t nBytes);
+        
+        /// Writes a single 8-bit value to a register
+        ssize_t writeRegister(uint8_t reg, uint8_t data);
+        
+        /// Reads data from device memory into a buffer
+        ssize_t readMemory(uint16_t offset, uintptr_t buffer, size_t nBytes);
+        
+        /// Writes data from a buffer into device memory
+        ssize_t writeMemory(uint16_t offset, uintptr_t buffer, size_t nBytes);
+        
+        /// Writes a single 8-bit value into device memory
+        ssize_t writeMemory(uint16_t offset, uint8_t data);
+
+        Dm9601(const Dm9601&);
+        void operator =(const Dm9601&);
+};
+
+#endif
 
