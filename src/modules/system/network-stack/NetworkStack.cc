d7505f029f1d595b0fbcb1a97e37be234c5e2178
Matthew Iselin <matthew@theiselins.net>
2010-08-08 20:57:53 +1000

    Avoid using the heap for the "safe packet" buffer in NetworkStack::receive.

--- src/modules/system/network-stack/NetworkStack.cc
+++ src/modules/system/network-stack/NetworkStack.cc
@@ -66,16 +66,15 @@ uint64_t NetworkStack::executeRequest(uint64_t p1, uint64_t p2, uint64_t p3, uin
     if(!packet || !packetSize)
         return 0;
 
-  // Pass onto the ethernet layer
-  /// \todo We should accept a parameter here that specifies the type of packet
-  ///       so we can pass it on to the correct handler, rather than assuming
-  ///       Ethernet.
-  Ethernet::instance().receive(packetSize, packet, pCard, offset);
+    // Pass onto the ethernet layer
+    /// \todo We should accept a parameter here that specifies the type of packet
+    ///       so we can pass it on to the correct handler, rather than assuming
+    ///       Ethernet.
+    Ethernet::instance().receive(packetSize, packet, pCard, offset);
 
-  uint8_t *pack = reinterpret_cast<uint8_t*>(packet);
-  delete [] pack;
+    m_MemPool.free(packet);
 
-  return 0;
+    return 0;
 }
 
 void NetworkStack::receive(size_t nBytes, uintptr_t packet, Network* pCard, uint32_t offset)
@@ -87,7 +86,7 @@ void NetworkStack::receive(size_t nBytes, uintptr_t packet, Network* pCard, uint
 
   // Some cards might be giving us a DMA address or something, so we copy
   // before passing on to the worker thread...
-  uint8_t *safePacket = new uint8_t[nBytes + 1];
+  uint8_t *safePacket = reinterpret_cast<uint8_t*>(m_MemPool.allocate());
   memcpy(safePacket, reinterpret_cast<void*>(packet), nBytes);
   addAsyncRequest(0, reinterpret_cast<uint64_t>(safePacket),
                      static_cast<uint64_t>(nBytes),
