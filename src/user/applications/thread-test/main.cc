cb52dced824f1778938df50971ab18835c528941
Matthew Iselin <matthew@theiselins.net>
2010-08-25 11:41:33 +1000

    (slightly modified) pthread mutex/spinlock test from http://www.alexonlinux.com/pthread-mutex-vs-pthread-spinlock

---
+++ src/user/applications/thread-test/main.cc
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/time.h>
+
+#include <list>
+
+#define LOOPS 1024 // 10000000
+
+// Modified from http://www.alexonlinux.com/do-you-need-mutex-to-protect-int
+// Uses mutexes or spinlocks
+
+using namespace std;
+
+list<int> the_list;
+
+#define USE_SPINLOCK
+
+#ifdef USE_SPINLOCK
+pthread_spinlock_t spinlock;
+#else
+pthread_mutex_t mutex;
+#endif
+
+void *consumer(void *ptr)
+{
+    int i;
+
+    printf("Consumer TID %lu\n", (unsigned long) pthread_self());
+
+    while (1)
+    {
+#ifdef USE_SPINLOCK
+        pthread_spin_lock(&spinlock);
+#else
+        pthread_mutex_lock(&mutex);
+#endif
+
+        if (the_list.empty())
+        {
+#ifdef USE_SPINLOCK
+            pthread_spin_unlock(&spinlock);
+#else
+            pthread_mutex_unlock(&mutex);
+#endif
+            break;
+        }
+
+        i = the_list.front();
+        the_list.pop_front();
+        printf("popping off front [%d]\n", pthread_self());
+
+#ifdef USE_SPINLOCK
+        pthread_spin_unlock(&spinlock);
+#else
+        pthread_mutex_unlock(&mutex);
+#endif
+    }
+    
+    printf("Thread done!\n");
+
+    return NULL;
+}
+
+int main()
+{
+    int i;
+    pthread_t thr1, thr2;
+    struct timeval tv1, tv2;
+
+#ifdef USE_SPINLOCK
+    pthread_spin_init(&spinlock, 0);
+#else
+    pthread_mutex_init(&mutex, NULL);
+#endif
+
+    // Creating the list content...
+    for (i = 0; i < LOOPS; i++)
+        the_list.push_back(i);
+
+    // Measuring time before starting the threads...
+    gettimeofday(&tv1, NULL);
+
+    pthread_create(&thr1, NULL, consumer, NULL);
+    pthread_create(&thr2, NULL, consumer, NULL);
+
+    pthread_join(thr1, NULL);
+    pthread_join(thr2, NULL);
+
+    // Measuring time after threads finished...
+    gettimeofday(&tv2, NULL);
+
+    if (tv1.tv_usec > tv2.tv_usec)
+    {
+        tv2.tv_sec--;
+        tv2.tv_usec += 1000000;
+    }
+
+    printf("Result - %ld.%ld\n", tv2.tv_sec - tv1.tv_sec,
+        tv2.tv_usec - tv1.tv_usec);
+
+#ifdef USE_SPINLOCK
+    pthread_spin_destroy(&spinlock);
+#else
+    pthread_mutex_destroy(&mutex);
+#endif
+
+    return 0;
+}
