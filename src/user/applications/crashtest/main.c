c4345f4703e8441cbcc8e2e41be9a5e8848ab756
Matthew Iselin <matthew@theiselins.net>
2010-09-06 17:44:25 +1000

    Added a new app "crashtest", which tests SIGSEGV, SIGFPE and SIGILL. Doesn't make it past SIGSEGV at the moment.

---
+++ src/user/applications/crashtest/main.c
@@ -0,0 +1,47 @@
+// crashtest: tests exception handling in the host POSIX subsystem
+
+#include <stdio.h>
+#include <signal.h>
+#include <string.h>
+
+// Don't give a warning for division by zero
+#pragma GCC diagnostic ignored "-Wdiv-by-zero"
+
+void sighandler(int sig)
+{
+    printf("Happily ignoring signal %d\n", sig);
+}
+
+int main(int argc, char *argv[])
+{
+    printf("crashtest: a return value of zero means success\n");
+    
+    signal(SIGSEGV, sighandler);
+    signal(SIGFPE, sighandler);
+    signal(SIGILL, sighandler);
+    
+    // SIGSEGV
+    printf("Testing SIGSEGV...\n");
+    char *p = (char*) 0;
+    strcpy(p, "Hello");
+    printf("Works.\n");
+    
+    // SIGFPE
+    printf("Testing SIGFPE...\n");
+    int a = 1 / 0;
+    float b = 1.0f / 0.0f;
+    double c = 1.0 / 0.0;
+    printf("Works.\n");
+    
+    // SIGILL
+    printf("Testing SIGILL...\n");
+    char badops[] = {0xab, 0xcd, 0xef, 0x12};
+    void (*f)() = (void (*)()) badops;
+    f();
+    printf("Works.\n");
+    
+    printf("All signals handled.\n");
+    
+    return 0;
+}
+
