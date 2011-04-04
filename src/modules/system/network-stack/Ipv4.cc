886780bf10ee29ea9c8a11603d78eae6bdbf5430
Matthew Iselin <matthew@theiselins.net>
2011-04-04 15:14:03 +1000

    IPv4: removed a line of debugging I forgot about.

--- src/modules/system/network-stack/Ipv4.cc
+++ src/modules/system/network-stack/Ipv4.cc
@@ -125,8 +125,6 @@ bool Ipv4::send(IpAddress dest, IpAddress from, uint8_t type, size_t nBytes, uin
 {
   IpAddress realDest = dest;
 
-  NOTICE_NOLOCK("outgoing ipv4 packet to " << dest.toString());
-
   // Grab the address to send to (as well as the NIC to send with)
   if(!pCard)
   {
