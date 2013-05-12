34c609581e1c68996a8ff3d18f7563dc14ed4a48
Matthew Iselin <matthew@theiselins.net>
2013-05-12 13:14:44 +1000

    posix: fix endian.h to actually define __BYTE_ORDER to something sane, and to have __LITTLE_ENDIAN and __BIG_ENDIAN be sane.

--- src/subsys/posix/include/machine/endian.h
+++ src/subsys/posix/include/machine/endian.h
@@ -1,20 +1,20 @@
 #ifndef __MACHINE_ENDIAN_H__
+#define __MACHINE_ENDIAN_H__
 
 #include <sys/config.h>
 
-#ifndef BIG_ENDIAN
-#define BIG_ENDIAN 4321
-#endif
-#ifndef LITTLE_ENDIAN
-#define LITTLE_ENDIAN 1234
+#define __LITTLE_ENDIAN 1234
+#define __BIG_ENDIAN 4321
+
+#define LITTLE_ENDIAN __LITTLE_ENDIAN
+#define BIG_ENDIAN __BIG_ENDIAN
+
+#ifdef __IEEE_LITTLE_ENDIAN
+#define __BYTE_ORDER __LITTLE_ENDIAN
+#else
+#define __BYTE_ORDER __BIG_ENDIAN
 #endif
 
-#ifndef BYTE_ORDER
-#ifdef __IEEE_LITTLE_ENDIAN
-#define BYTE_ORDER LITTLE_ENDIAN
-#else
-#define BYTE_ORDER BIG_ENDIAN
-#endif
-#endif
+#define BYTE_ORDER __BYTE_ORDER
 
 #endif /* __MACHINE_ENDIAN_H__ */
