d0118b579cde08c3c71f575c1188eb8dc2cd59e3
Matthew Iselin <matthew@theiselins.net>
2013-05-16 12:14:55 +1000

    user: new apps 'init' and 'preloadd'. * init: main startup binary that can then load the window manager, etc * preloadd: app that sits in the background and reads in a bunch of common apps (ls, bash, etc) so they're hot in the cache for the user

---
+++ src/user/applications/preloadd/main.c
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2013 Matthew Iselin
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <syslog.h>
+#include <ctype.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sched.h>
+
+const char *g_FilesToPreload[] = {
+    "/applications/bash",
+    "/applications/ls",
+    "/applications/cat",
+    "/applications/make",
+    "/applications/gcc",
+    "/applications/ld",
+    "/support/gcc/libexec/gcc/i686-pedigree/4.5.1/cc1",
+    "/support/gcc/libexec/gcc/i686-pedigree/4.5.1/cc1plus",
+    "/support/gcc/libexec/gcc/i686-pedigree/4.5.1/collect2",
+    "/libraries/libc.so",
+    "/libraries/libm.so",
+    "/libraries/libgmp.so",
+    "/libraries/libmpfr.so",
+    "/libraries/libmpc.so",
+    0
+};
+
+int main(int argc, char **argv)
+{
+  syslog(LOG_INFO, "preloadd: starting...");
+
+  size_t n = 0;
+  const char *s = g_FilesToPreload[n++];
+  do
+  {
+    struct stat st;
+    int e = stat(s, &st);
+    if(e == 0)
+    {
+      syslog(LOG_INFO, "preloadd: preloading %s...", s);
+      FILE *fp = fopen(s, "rb");
+      char *buf = (char *) malloc(st.st_size);
+      fread(buf, st.st_size, 1, fp);
+      free(buf);
+      fclose(fp);
+      syslog(LOG_INFO, "preloadd: preloading %s complete!", s);
+    }
+    else
+    {
+      syslog(LOG_INFO, "preloadd: %s probably does not exist", s);
+    }
+    s = g_FilesToPreload[n++];
+  } while(s);
+
+  /// \todo Fix bug with orphaned processes exiting.
+  while(1) sched_yield();
+  return 0;
+}
