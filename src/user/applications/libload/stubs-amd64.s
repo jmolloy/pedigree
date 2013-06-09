375eaf4007d083cab3f666567e389e74bd4f17ed
Matthew Iselin <matthew@theiselins.net>
2013-06-09 14:49:28 +1000

    libload: on amd64, save caller-save registers before doing a symbol fixup (fixes eg write() being called with the wrong parameters)

--- src/user/applications/libload/stubs-amd64.s
+++ src/user/applications/libload/stubs-amd64.s
@@ -3,17 +3,32 @@
 [extern _libload_dofixup]
 
 _libload_resolve_symbol:
-    pop r15
-    pop r14
+    ; pop r15
+    ; pop r14
 
+    ; Save caller-save registers.
+    push rcx
+    push rdx
     push rdi
     push rsi
+    push r8
+    push r9
+    push r10
+    push r11
 
-    mov rdi, r15
-    mov rsi, r14
+    mov rdi, [rsp + 64]
+    mov rsi, [rsp + 72]
     call _libload_dofixup
 
+    ; Restore caller-save registers.
+    pop r11
+    pop r10
+    pop r9
+    pop r8
     pop rsi
     pop rdi
+    pop rdx
+    pop rcx
 
+    add rsp, 16
     jmp rax
