13d19442905af713e566931d371a34b1f31cd58d
Matthew Iselin <matthew@theiselins.net>
2014-05-01 09:35:15 +1000

    build: pull in shared libstdc++ to images/local and minimise effort when building libstdc++ in compiler build.

--- easy_build_x64.sh
+++ easy_build_x64.sh
@@ -132,7 +132,24 @@ set -e
 # Run a quick build of libc and libm for the rest of the build system.
 scons CROSS=$script_dir/compilers/dir/bin/x86_64-pedigree- build/libc.so build/libm.so
 
+# Pull down libtool.
+echo
+echo "Configuring the Pedigree UPdater..."
+
+$script_dir/setup_pup.py amd64
+$script_dir/run_pup.py sync
+
+$script_dir/run_pup.py install libtool
+
+# Enforce using our libtool.
+export LIBTOOL=$script_dir/../images/local/applications:$PATH
+
 # Build GCC again with access to the newly built libc.
+# This will create a libstdc++ that can be used by pedigree-apps to build GCC
+# again, this time with a shared libstdc++. pedigree-apps should then build GCC
+# again to build it against the shared libstdc++. Once a working shared
+# libstdc++ exists, the static one built here is no longer relevant.
+# What a mess!
 $script_dir/scripts/checkBuildSystemNoInteractive.pl x86_64-pedigree $script_dir/pedigree-compiler $compiler_build_options "libcpp"
 
 set +e
@@ -143,12 +160,6 @@ echo "Ensuring CDI is up-to-date."
 # Setup all submodules, make sure they are up-to-date
 git submodule init > /dev/null 2>&1
 git submodule update > /dev/null 2>&1
-
-echo
-echo "Configuring the Pedigree UPdater..."
-
-$script_dir/setup_pup.py amd64
-$script_dir/run_pup.py sync
 
 echo
 echo "Installing a base set of packages..."
@@ -166,6 +177,9 @@ $script_dir/run_pup.py install pixman
 $script_dir/run_pup.py install cairo
 $script_dir/run_pup.py install expat
 $script_dir/run_pup.py install mesa
+
+# Install GCC to pull in shared libstdc++.
+$script_dir/run_pup.py install gcc
 
 set -e
 
