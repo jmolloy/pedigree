369c6fd4b7a7eff310a561aa0c987b2130e163d6
Matthew Iselin <matthew@theiselins.net>
2014-06-12 12:03:23 +1000

    pup: got my min and my max confused

--- scripts/pup/pup-install.py
+++ scripts/pup/pup-install.py
@@ -18,26 +18,6 @@ ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 '''
 
-#pylint: disable
-'''
-Copyright (c) 2008-2014, Pedigree Developers
-
-Please see the CONTRIB file in the root of the source tree for a full
-list of contributors.
-
-Permission to use, copy, modify, and distribute this software for any
-purpose with or without fee is hereby granted, provided that the above
-copyright notice and this permission notice appear in all copies.
-
-THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-'''
-
 import os
 import sys
 import stat
@@ -198,7 +178,7 @@ def main(arglist):
     files = [f for f in filelist if not f.linkname]
     links = [f for f in filelist if f.linkname]
 
-    p = multiprocessing.pool.Pool(processes=min(multiprocessing.cpu_count() / 2, 1))
+    p = multiprocessing.pool.Pool(processes=max(multiprocessing.cpu_count() / 2, 1))
     p.map(do_extract, files)
 
     # Do links last and unthreaded, as they depend on extracted files.
