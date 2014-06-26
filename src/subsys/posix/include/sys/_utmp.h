50698710dc05a1b625110c1ce85c74116c4da69a
Matthew Iselin <matthew@theiselins.net>
2014-06-26 15:02:34 +1000

    posix: further fixes to utmp header(s)

--- src/subsys/posix/include/sys/_utmp.h
+++ src/subsys/posix/include/sys/_utmp.h
@@ -17,8 +17,8 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-#ifndef _SYS_UTMPX_H
-#define _SYS_UTMPX_H
+#ifndef _SYS_UTMP_H
+#define _SYS_UTMP_H
 
 #include <sys/types.h>
 #include <sys/time.h>
@@ -32,17 +32,6 @@
 #define UT_NAMESIZE     64
 #define UT_HOSTSIZE     256
 
-struct __UTMP_STRUCT {
-    char ut_user[UT_NAMESIZE];
-    char ut_id[UT_LINESIZE];
-    char ut_line[UT_LINESIZE];
-    pid_t ut_pid;
-    short ut_type;
-    struct timeval ut_tv;
-};
-
-#define ut_name ut_user
-
 #define EMPTY           0
 #define RUN_LVL         1
 #define BOOT_TIME       2
@@ -53,4 +42,19 @@ struct __UTMP_STRUCT {
 #define LOGIN_PROCESS   7
 #define DEAD_PROCESS    8
 
+#endif
+
+#ifdef __UTMP_STRUCT
+struct __UTMP_STRUCT {
+    char ut_user[UT_NAMESIZE];
+    char ut_id[UT_LINESIZE];
+    char ut_line[UT_LINESIZE];
+    pid_t ut_pid;
+    short ut_type;
+    struct timeval ut_tv;
+};
+#endif
+
+#ifndef ut_name
+#define ut_name ut_user
 #endif
