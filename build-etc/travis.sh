26e956a3fe44add678776aa6df59c24377a7bf81
Matthew Iselin <matthew@theiselins.net>
2015-07-30 13:41:25 +1000

    build: fix up travis compiler directory for cache

--- build-etc/travis.sh
+++ build-etc/travis.sh
@@ -5,6 +5,9 @@ TRAVIS_OPTIONS=
 if [ ! -z "$TRAVIS" ]; then
     TRAVIS_OPTIONS="travis=$TRAVIS forcemtools=1"
 
+    # Override the compiler directory for Travis caching.
+    COMPILER_DIR=$HOME/pedigree-compiler
+
     # The toolchain .deb file installs all files owned as root, which is not
     # useful when we want to symlink our crt* and POSIX headers. So, fix that.
     ME=`whoami`
