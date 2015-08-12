15910814c5b4f9d7895eb9471ea251471f67f1a7
Matthew Iselin <matthew@theiselins.net>
2015-08-12 15:12:44 +1000

    thread-test: tests for recursive mutexes and deadlocks

--- src/user/applications/thread-test/main.cc
+++ src/user/applications/thread-test/main.cc
@@ -1,5 +1,4 @@
 /*
- * 
  * Copyright (c) 2008-2014, Pedigree Developers
  *
  * Please see the CONTRIB file in the root of the source tree for a full
@@ -22,7 +21,9 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <errno.h>
+#include <string.h>
 #include <sys/time.h>
+#include <syslog.h>
 
 #include <list>
 
@@ -92,6 +93,43 @@ int main()
     pthread_mutex_init(&mutex, NULL);
 #endif
 
+    // syslog used to split up debug logs.
+    syslog(LOG_INFO, "TEST 0");
+    printf("Locking with deadlock\n");
+    errno = 0;
+    i = pthread_mutex_lock(&mutex);
+    printf("First lock: %d (%s)\n", i, strerror(errno));
+    i = pthread_mutex_lock(&mutex);
+    if (errno != EDEADLK) printf("Didn't get EDEADLK!\n");
+    printf("Second lock: %d (%s)\n", i, strerror(errno));
+    pthread_mutex_unlock(&mutex);
+
+    syslog(LOG_INFO, "TEST 1");
+
+    printf("Locking without any contention...\n");
+    pthread_mutex_lock(&mutex);
+    printf("Acquired\n");
+    pthread_mutex_unlock(&mutex);
+    printf("Released!\n");
+
+    syslog(LOG_INFO, "TEST 2");
+
+    printf("Creating a recursive lock!\n");
+    pthread_mutex_t recursive;
+    pthread_mutexattr_t attr;
+    pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE);
+    pthread_mutex_init(&recursive, &attr);
+    printf("Testing recursion...\n");
+    pthread_mutex_lock(&recursive);
+    pthread_mutex_lock(&recursive);
+    pthread_mutex_lock(&recursive);
+    printf("Locked OK, testing unlock...\n");
+    pthread_mutex_unlock(&recursive);
+    pthread_mutex_unlock(&recursive);
+    pthread_mutex_unlock(&recursive);
+    pthread_mutex_unlock(&recursive);
+    printf("OK!\n");
+
     // Creating the list content...
     for (i = 0; i < LOOPS; i++)
         the_list.push_back(i);
@@ -99,11 +137,13 @@ int main()
     // Measuring time before starting the threads...
     gettimeofday(&tv1, NULL);
 
+    syslog(LOG_INFO, "TEST 3");
     pthread_create(&thr1, NULL, consumer, NULL);
     pthread_create(&thr2, NULL, consumer, NULL);
 
     pthread_join(thr1, NULL);
     pthread_join(thr2, NULL);
+    syslog(LOG_INFO, "TEST 4");
 
     // Measuring time after threads finished...
     gettimeofday(&tv2, NULL);
