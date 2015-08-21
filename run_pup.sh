2d6cc502913f6661cf8e255d1a7bc92a781ed2e4
Matthew Iselin <matthew@theiselins.net>
2015-08-21 14:56:41 +1000

    pup: use pup wheel from pup.pedigree-project.org for pup

---
+++ run_pup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Run pup, and install pup if it is not present.
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+if [ ! -e "$DIR/scripts/pup.whl" ]; then
+  # TODO(miselin): figure out just how to update this later.
+  curl -o "$DIR/scripts/pup.whl" http://pup.pedigree-project.org/pup.whl
+fi
+
+python "$DIR/scripts/pup.whl/pedigree_updater" --config="$DIR/scripts/pup/pup.conf" $*
+
