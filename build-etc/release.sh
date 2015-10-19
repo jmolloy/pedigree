9af3fb03c7bc0bf9449b6d677e0440ce7e93b645
Matthew Iselin <matthew@theiselins.net>
2015-10-19 00:41:46 -0700

    build-etc: added a script to simplify turning ISOs into releases

---
+++ build-etc/release.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]; then
+    echo "Usage: release.sh build_path [output_dir] [name]" >&2
+    exit 1
+fi
+
+RELEASE=1
+if [ -z "$KEYCDN_KEYPATH" ]; then
+    echo "KEYCDN_KEYPATH is empty, won't be able to upload releases." >&2
+    RELEASE=0
+fi
+
+BUILDPATH="$1"
+
+OUTPUTDIR="/tmp/pedigree-release"
+if [ ! -z "$2" ]; then
+    OUTPUTDIR="$2"
+fi
+
+DATESTAMP=`date "+%Y%m%d"`
+RELEASE_NAME="pedigree-$DATESTAMP"
+if [ ! -z "$3" ]; then
+    RELEASE_NAME="$3"
+fi
+
+# Get ourselves a temporary directory to work in.
+rm -rf $OUTPUTDIR
+mkdir -p $OUTPUTDIR
+
+# Bring across the main, minted ISO.
+cp "$BUILDPATH"/pedigree.iso $OUTPUTDIR/
+
+# Compress as needed.
+gzip --keep --rsyncable $OUTPUTDIR/pedigree.iso
+bzip2 --keep $OUTPUTDIR/pedigree.iso
+xz --keep $OUTPUTDIR/pedigree.iso
+
+# Now done with the original image.
+rm -f $OUTPUTDIR/pedigree.iso
+
+# Final step: turn into release names.
+rename "s/pedigree(\..*)*$/$RELEASE_NAME\$1/" $OUTPUTDIR/*
+
+pushd $OUTPUTDIR
+# Checksum all images.
+md5sum "$RELEASE_NAME"* >md5sums
+sha1sum "$RELEASE_NAME"* >sha1sums
+sha256sum "$RELEASE_NAME"* >sha256sums
+popd
+
+echo "Release files are now present at $OUTPUTDIR."
+
+if [ $RELEASE -gt 0 ]; then
+    echo "Uploading releases..."
+    rsync -av --chmod=Du=rwx,Dgo=rx,Fu=rw,Fgo=r -e "ssh -i $KEYCDN_KEYPATH" \
+        $OUTPUTDIR/* miselin@rsync.keycdn.com:zones/download/
+fi
