2227201bd339d6bd0b8f9206b01ed88f1820a5fb
Matthew Iselin <matthew@theiselins.net>
2015-11-17 13:57:14 +1100

    scripts: addr2line.py now handles inlines better

--- scripts/addr2line.py
+++ scripts/addr2line.py
@@ -1,4 +1,22 @@
-#!/usr/bin/env python
+'''
+Copyright (c) 2008-2014, Pedigree Developers
+
+Please see the CONTRIB file in the root of the source tree for a full
+list of contributors.
+
+Permission to use, copy, modify, and distribute this software for any
+purpose with or without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+'''
+
 from __future__ import print_function
 
 # Wrapper script to perform addr2line, using data from the serial log.
@@ -82,7 +100,7 @@ def main():
                 if not os.path.isfile(filename):
                     continue
 
-            os.system('addr2line -p -C -f -e %s 0x%x' % (filename, address + addend))
+            os.system('addr2line -p -C -f -i -e %s 0x%x' % (filename, address + addend))
 
 
     return 0
