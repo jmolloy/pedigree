1512588e39bac599c6e31ce2119895254bab1f88
Matthew Iselin <matthew@theiselins.net>
2016-01-06 17:17:28 +1100

    scripts: remove buildLib{c,m}.py and buildDb.py, replace with SCons calls
    This just removes the extra subprocesses and the additional variables that had to be sent to them just to build.

---
+++ site_scons/buildutils/libs.py
@@ -0,0 +1,98 @@
+'''
+Copyright (c) 2008-2014, Pedigree Developers
+
+Please see the CONTRIB file in the root of the source tree for a full
+list of contributors.
+
+Permission to use, copy, modify, and distribute this software for any
+purpose with or without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+'''
+
+import os
+import subprocess
+import tarfile
+
+import SCons
+
+
+def buildLibc(env, libc_in):
+  build_dir = env["PEDIGREE_BUILD_BASE"]
+  images_dir = env["PEDIGREE_IMAGES_DIR"]
+  env["PEDIGREE_IMAGES_DIR"] = env.Dir(images_dir).path
+
+  libc_static_out = os.path.join(build_dir, "libc.a")
+  libg_static_out = os.path.join(build_dir, "libg.a")
+  libg_static_tmp = os.path.join(build_dir, "libg_.a")
+
+  libc_shared_out = os.path.join(build_dir, "libc.so")
+  libg_shared_out = os.path.join(build_dir, "libg.so")
+
+  # Bring across libg.a so we don't modify the actual master copy
+  # TODO(miselin): this only works on *nix
+  libg_static = env.Command(libg_static_tmp, libc_in, "cp $SOURCE $TARGET")
+
+  # Set of object files to remove from the library. These are generally things
+  # that Pedigree provides.
+  objs_to_remove = [
+      "init", "getpwent", "signal", "fseek", "getcwd", "rewinddir", "opendir",
+      "readdir", "closedir", "_isatty", "basename", "setjmp", "malloc",
+      "mallocr", "calloc", "callocr", "free", "freer", "realloc", "reallocr",
+      "malign", "malignr", "mallinfor", "mstats", "mtrim", "valloc", "msize",
+      "mallinfor", "malloptr", "mallstatsr", "ttyname", "strcasecmp",
+      "memset"]
+
+  # What target are we using?
+  if env["ON_PEDIGREE"]:
+    return
+  else:
+    if env["ARCH_TARGET"] in ["X64"]:
+      objs_to_remove.extend(["memcpy",])
+    elif env["ARCH_TARGET"] in ["ARM"]:
+      objs_to_remove.extend(["access",])
+
+  deletions = ["lib_a-%s.o" % (x,) for x in objs_to_remove]
+
+  # Remove the object files we manually override in our glue.
+  libg = (
+      env.Command(
+          libg_static_out, libg_static_tmp,
+          "mv $SOURCE $TARGET && $AR d $TARGET %s" %
+          (" ".join(deletions),)))
+
+  # Build the shared object.
+  env.Command(
+      libg_shared_out, libg_static_out,
+      "$LINK -nostdlib -shared -g3 -ggdb -gdwarf-2 -Wl,-soname,libg.so -L. "
+      "-L$PEDIGREE_BUILD_BASE -L$PEDIGREE_IMAGES_DIR/libraries -Wl,-Bstatic "
+      "-Wl,--whole-archive $SOURCE -lpedigree-glue -lpedigree-c "
+      "-Wl,--no-whole-archive -Wl,-Bdynamic -lgcc -lncurses -o $TARGET")
+  env.Command(
+      libc_shared_out, libg_static_out,
+      "$LINK -nostdlib -shared -Wl,-soname,libc.so -L. -L$PEDIGREE_BUILD_BASE"
+      "  -L$PEDIGREE_IMAGES_DIR/libraries -Wl,-Bstatic -Wl,--whole-archive "
+      "$SOURCE -lpedigree-glue -lpedigree-c -Wl,--no-whole-archive "
+      "-Wl,-Bdynamic -lgcc -lncurses -o $TARGET")
+
+  # TODO(miselin): need to add glue to the libg.a we already have!
+
+
+def buildLibm(env, libm_in):
+  build_dir = env["PEDIGREE_BUILD_BASE"]
+
+  libm_static_in = os.path.join(build_dir, "stock-libm.a")
+  libm_static_out = os.path.join(build_dir, "libm.a")
+  libm_shared_out = os.path.join(build_dir, "libm.a")
+
+  env.Command(libm_static_out, libm_static_in, "cp $SOURCE $TARGET")
+  env.Command(libm_shared_out, libm_static_in,
+              "$LINK -nostdlib -shared -Wl,-sonamelibm.so -L. -o $TARGET "
+              "-Wl,--whole-archive $SOURCE -Wl,--no-whole-archive -lgcc")
