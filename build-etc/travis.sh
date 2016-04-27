20b639f649ea1592d79c4438ffd65741fc4051df
Matthew Iselin <matthew@theiselins.net>
2016-04-27 15:39:38 +1000

    build-etc: update the compilers path chown on travis

--- build-etc/travis.sh
+++ build-etc/travis.sh
@@ -11,5 +11,5 @@ if [ ! -z "$TRAVIS" ]; then
     # The toolchain .deb file installs all files owned as root, which is not
     # useful when we want to symlink our crt* and POSIX headers. So, fix that.
     ME=`whoami`
-    sudo chown -R "$ME" $script_dir/pedigree-compiler
+    sudo chown -R "$ME" $COMPILER_DIR
 fi
