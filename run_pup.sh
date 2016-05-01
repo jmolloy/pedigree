4b10f723f107c4d753e258d16c9a7618f0eb2533
Matthew Iselin <matthew@theiselins.net>
2016-05-01 06:28:55 -0700

    pup: reduce how often we try to download a new version of pup

--- run_pup.sh
+++ run_pup.sh
@@ -9,13 +9,25 @@ PUP="$DIR/scripts/pup.whl"
 
 set +e
 
+try_update_pup()
+{
+    curl -o ".pup-version-new" https://pup.pedigree-project.org/pup-version
+    if cmp --silent ".pup-version-new" ".pup-version"; then
+        rm -f ".pup-version-new"
+    else
+        curl -o "$PUP_TMP" https://pup.pedigree-project.org/pup.whl && \
+            mv "$PUP_TMP" "$PUP" && mv ".pup-version-new" ".pup-version"
+    fi
+}
+
+# Download pup for the first time if we don't know what version we have.
+if [ ! -e .pup-version ]; then
+    try_update_pup
+fi
+
 # Update pup if needed.
-curl -o ".pup-version-new" https://pup.pedigree-project.org/pup-version
-if cmp --silent ".pup-version-new" ".pup-version"; then
-    rm -f ".pup-version-new"
-else
-    curl -o "$PUP_TMP" https://pup.pedigree-project.org/pup.whl && \
-        mv "$PUP_TMP" "$PUP" && mv ".pup-version-new" ".pup-version"
+if test $(find .pup-version -maxdepth 1 -mmin +120); then
+    try_update_pup
 fi
 
 set -e
