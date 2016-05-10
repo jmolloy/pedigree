a9700da93652c88c2a092fb24f28e5d8e16c9808
Matthew Iselin <matthew@theiselins.net>
2016-05-09 18:52:54 -0700

    x86: clean up memory regions when shutting down

--- src/system/kernel/core/processor/x86_common/PhysicalMemoryManager.cc
+++ src/system/kernel/core/processor/x86_common/PhysicalMemoryManager.cc
@@ -383,6 +383,7 @@ bool X86CommonPhysicalMemoryManager::allocateRegion(MemoryRegion &Region,
 void X86CommonPhysicalMemoryManager::shutdown()
 {
     NOTICE("Shutting down X86CommonPhysicalMemoryManager");
+    PhysicalMemoryManager::m_MemoryRegions.clear();
     m_PageMetadata.clear();
 }
 
